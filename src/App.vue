4d8bb5c576290b07f3b458a24e7da5fa25f05195
Juan David Alvarez Gamez <juandavidalvarezgamez@gmail.com>
2020-12-13 22:36:44 -0500

    Se crea en Vue la estructura principal de la interfaz, un header con algunas opciones, la sección principal donde luego se añadirán algunas opciones de los componentes y un footer. Se tendra un usuario precargado, simulando que se ha iniciado sesión en localstorade, se edita un template, el script, el style.

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,131 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    
+  <div id="app">
+        
+    <div class="header">
+            
+      <h1>Banco BWT5</h1>
+            
+      <nav>
+                <button v-on:click="init" v-if="is_auth"> Inicio </button>
+                <button v-on:click="getBalance" v-if="is_auth"> Saldo </button>
+                <button v-if="is_auth"> Transacción </button>        
+        <button v-if="is_auth">Cerrar Sesión</button>
+      </nav>
+          
+    </div>
+     
+    <div class="main-component">         </div>
+    <div class="footer">
+            
+      <h2>ParqueSoft 2020</h2>
+          
+    </div>
+  </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
 
-@Options({
-  components: {
-    HelloWorld,
+<script>
+export default {
+  name: "App",
+  components: {},
+  data: function () {
+    return {
+      is_auth: localStorage.getItem("isAuth") || false,
+    };
   },
-})
-export default class App extends Vue {}
+  methods: {},
+  beforeCreate: function () {
+    localStorage.setItem("current_username", "camilo24");
+    localStorage.setItem("isAuth", true);
+  },
+};
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
+
+<style>
+  body{
+    margin: 0 0 0 0;
+  }
+
+  .header{
+    margin: 0%;
+    padding: 0;
+    width: 100%;
+    height: 10vh;
+    min-height: 100px;
+
+    background-color: #283747 ;
+    color:#E5E7E9  ;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+
+  .header h1{
+    width: 20%;
+    text-align: center;
+  }
+
+  .header nav {
+    height: 100%;
+    width: 45%;
+
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    font-size: 20px;
+  }
+
+  .header nav button{
+    color: #E5E7E9;
+    background: #283747;
+    border: 1px solid #E5E7E9;
+
+    border-radius: 5px;
+    padding: 10px 20px;
+  }
+
+  .header nav button:hover{
+    color: #283747;
+    background: #E5E7E9;
+    border: 1px solid #E5E7E9;
+  }
+
+  .main-component{
+    height: 75vh;
+    margin: 0%;
+    padding: 0%;
+
+    background: #FDFEFE ;
+  }
+
+
+  .footer{
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 10vh;
+    min-height: 100px;
+
+    background-color: #283747;
+    color: #E5E7E9;
+
+  }
+
+  .footer h2{
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+
 </style>
